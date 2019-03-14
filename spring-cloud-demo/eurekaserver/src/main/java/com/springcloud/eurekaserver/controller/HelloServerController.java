f103231870dadd5356a7d54caed55dedafb5fec6
weiminfang <minfang.wei@newtouch.cn>
2019-03-14 15:54:01 +0800

    add 知识点文件

---
+++ spring-cloud-demo/eurekaserver/src/main/java/com/springcloud/eurekaserver/controller/HelloServerController.java
@@ -0,0 +1,18 @@
+package com.springcloud.eurekaserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloServerController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello,this is hello-service";
+    }
+
+//    @GetMapping("/eureka")
+//    public String h1() {
+//        return "hello,this is eureka server";
+//    }
+}
